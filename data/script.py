934e3560dc8fc6b1ee8f15d897c8fb91da8a9705
dbaues <dbaues1@gmail.com>
2021-04-08 20:23:03 -0500

    Update script

--- data/script.py
+++ data/script.py
@@ -1,22 +1,24 @@
 from random import randint
 from random import sample
 
+"""
+ Creates a data file of random numbers for inserting and removing numbers
+ for 2021 DSU CSC310 Advanced Data Structures AVL Tree.
+
+ Created by Dan Bauer (2021)
+"""
+
+# File Values.
 N_SIZE = 50
 MIN = 1
-MAX = 500
-FILE_NAME = "tmp9.dat"
+MAX = 100
+FILE_NAME = "tmp10.dat"
 
-list = []
-list2 = []
-""" #DEPRECATED
-while len(list) < N_SIZE:
-    x = randint(MIN, MAX)
-    if x not in list:
-        list.append(x)
-"""
+# Creates random samples.
 list = sample(range(MIN,MAX),N_SIZE)
 list2 = sample(list, int(N_SIZE/2))
 
+# Generates the data file.
 file = open(FILE_NAME, 'w')
 for i in list:
     file.writelines("0\n")
@@ -28,5 +30,6 @@ for i in list2:
     file.writelines(str(i) + "\n")
 
 file.write("2\n")
+file.write("8\n")
 file.write("9\n")
 file.close()
